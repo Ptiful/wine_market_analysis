642d885c18f1d0a927621e3207ebd8b627eb80ce
Ptiful <strazzu@gmail.com>
2023-06-16 12:23:51 +0200

    adding src folder

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ chart1, chart2 = st.columns(2)
 with chart1:
     st.subheader(body="10 wines to increase sales")
     ten_wines_to_increase = pd.read_sql_query(
-        open("1 - 10_wines_to_increse_sales.sql", "r").read(),
+        open("src/1 - 10_wines_to_increse_sales.sql", "r").read(),
         conn,
     )
     st.dataframe(ten_wines_to_increase, hide_index=True)
@@ -64,7 +64,7 @@ with chart1:
 with chart2:
     st.subheader(body="Country to prioritize")
     country_to_prioritize = pd.read_sql_query(
-        open("2 - Country_to_prioritize.sql", "r").read(), conn
+        open("src/2 - Country_to_prioritize.sql", "r").read(), conn
     )
     st.dataframe(country_to_prioritize, hide_index=True)
     st.markdown(
@@ -76,7 +76,7 @@ st.markdown("<hr>", unsafe_allow_html=True)
 # Making charts3
 st.subheader(body="Price for the best winery")
 price_for_the_best_winery = pd.read_sql_query(
-    open("3 - Price_to_best_winery.sql", "r").read(),
+    open("src/3 - Price_to_best_winery.sql", "r").read(),
     conn,
 )
 st.dataframe(price_for_the_best_winery, hide_index=True, width=1700)
@@ -86,7 +86,7 @@ st.markdown("<hr>", unsafe_allow_html=True)
 # Making chart4
 st.subheader(body="Arome")
 wine_keywords_by_10_users = pd.read_sql_query(
-    open("4 - Wine_keywords_by_10_users.sql", "r").read(), conn
+    open("src/4 - Wine_keywords_by_10_users.sql", "r").read(), conn
 )
 st.dataframe(wine_keywords_by_10_users, hide_index=True, use_container_width=True)
 st.markdown(
@@ -100,7 +100,7 @@ chart3, chart4 = st.columns(2)
 with chart3:
     st.subheader(body="Grapes and wines")
     grapes_and_wines = pd.read_sql_query(
-        open("5 - Grapes_and_wines.sql", "r").read(), conn
+        open("src/5 - Grapes_and_wines.sql", "r").read(), conn
     )
     st.dataframe(grapes_and_wines, hide_index=True)
     st.markdown(body="You'll find here all your best grapes and wines from all time !")
@@ -109,7 +109,7 @@ st.markdown("<hr>", unsafe_allow_html=True)
 with chart4:
     st.subheader(body="Country Leaderbord")
     country_leaderbord = pd.read_sql_query(
-        open("6 - Country_leaderboard.sql", "r").read(), conn
+        open("src/6 - Country_leaderboard.sql", "r").read(), conn
     )
     st.dataframe(country_leaderbord, hide_index=True)
     st.markdown(body="That would be the average wine rating by country.")
@@ -120,7 +120,7 @@ chart5, chart6 = st.columns(2)
 with chart5:
     st.subheader(body="Country Leaderbord Vintages")
     country_leaderbord_vintages = pd.read_sql_query(
-        open("6.1 - Country_leaderboard_vintage.sql", "r").read(), conn
+        open("src/6.1 - Country_leaderboard_vintage.sql", "r").read(), conn
     )
     st.dataframe(country_leaderbord_vintages, hide_index=True)
     st.markdown(body="That would be the average vintage wine rating by country.")
@@ -129,7 +129,7 @@ with chart5:
 with chart6:
     st.subheader(body="Top 5 Carbent Sauvignon like")
     top_5_Cabernet_Sauvignon_like = pd.read_sql_query(
-        open("7 - Top_5_for_Sauvignon.sql", "r").read(), conn
+        open("src/7 - Top_5_for_Sauvignon.sql", "r").read(), conn
     )
     st.dataframe(top_5_Cabernet_Sauvignon_like, hide_index=True)
     st.markdown(
